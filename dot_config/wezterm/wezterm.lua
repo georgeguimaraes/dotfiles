ed5bd210c3e92e81a216b29838c6a6ad3f76961c
George Guimarães <george.guimaraes@gmail.com>
2025-08-18 10:59:14 -0300

    Update .config/wezterm/wezterm.lua Update .gitconfig Update .gitignore Update .zshrc

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -356,16 +356,6 @@ config.keys = {
     key = "g",
     action = action.PaneSelect({ mode = "SwapWithActive" }),
   },
-  {
-    key = "=",
-    mods = "CMD|SHIFT",
-    action = wezterm.action.ToggleFullScreen,
-  },
-  {
-    key = "Enter",
-    mods = "ALT",
-    action = wezterm.action.DisableDefaultAssignment,
-  },
 
   -- move between split panes
   split_nav("move", "h"),
